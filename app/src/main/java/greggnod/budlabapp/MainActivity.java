2f270fc069f7fcd324680e85dc3340a3bfae6a14
angadgarg25 <angad.garg3@gmail.com>
2016-09-17 15:49:13 -0500

    Finished Camera page.

--- app/src/main/java/greggnod/budlabapp/MainActivity.java
+++ app/src/main/java/greggnod/budlabapp/MainActivity.java
@@ -2,6 +2,7 @@ package greggnod.budlabapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void viewToMembers(View view){
+        setContentView(R.layout.members);
+    }
+
+    public void viewToCamera(View view){
+        setContentView(R.layout.camera);
     }
 }

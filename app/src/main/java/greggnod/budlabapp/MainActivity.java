f20c8f06fb792a524f44dc1d80e326bfad15b22b
angadgarg25 <angad.garg3@gmail.com>
2016-09-17 17:10:08 -0500

    Worked on Camera Intent.

--- app/src/main/java/greggnod/budlabapp/MainActivity.java
+++ app/src/main/java/greggnod/budlabapp/MainActivity.java
@@ -1,5 +1,10 @@
 package greggnod.budlabapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,5 +23,29 @@ public class MainActivity extends AppCompatActivity {
 
     public void viewToCamera(View view){
         setContentView(R.layout.camera);
+    }
+
+    public void cropCameraIntent(View view){
+        double latitude, longitude;
+        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, 1);
+        }
+    }
+
+    public void grainCameraIntent(View view){
+        double latitude, longitude;
+        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, 1);
+        }
     }
 }
